1762b7e77d843e41e6655eb0473b221b498480d2
Oleg_Zaharenko <zakharenka.oleg@gmail.com>
2022-05-31 21:12:24 +0300

    a

--- src/Lesson4.java
+++ src/Lesson4.java
@@ -6,6 +6,10 @@ public class Lesson4 {
         System.out.println(a);   //- переполнение
         int c = a + a;
         System.out.println(c); // присвоение переполненного byte переменной большего размера
+        double v = 2 * 3;
+        System.out.println(v);
+        double g = 2.0 * 3;
+        System.out.println(g);
         int[] d = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         for (int i = 0; i < d.length; i++) {            //различного рода операторы
@@ -20,11 +24,12 @@ public class Lesson4 {
                 }
             }
         }
-        for (int f = 0; f < d.length; f++) {
-            System.out.print(d[f] + " ");
+        for (int i = 0; i < d.length; i++) {
+            System.out.print(+d[i] + " ");
         }
-        System.out.println(a * a); //умножение
-        System.out.println(a/a); //деление;
+
+        System.out.println( "Умножение: " + a * a); //умножение
+        System.out.println("Деление: " + a/a); //деление;
 
     }
 }
